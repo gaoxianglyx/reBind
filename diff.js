bb83fe86cf67906d4a910aafaf1e794571ad6b21
gaoxiang <1251086707@qq.com>
2020-06-07 12:38:34 +0800

    'diff'

--- diff.js
+++ diff.js
@@ -1,11 +1,13 @@
-/*todo*/
+/*给你两个单词 word1 和 word2，请你计算出将 word1 转换成 word2 所使用的最少操作数
+你只能对一个单词进行插入/删除/替换一个字符
+*/
 function minDistance(word1, word2) {
     var l1 = word1.length, l2 = word2.length;
     var dp = new Array(l1 + 1);
 	for(var i = 0;i <=  l1; i++){
 			dp[i] = new Array( l2 + 1);
 	}
-	/*使用 二维 DP[i][j]，表示 word1 的 i 个字母 与 word2 的 第 j 个字母 相同 需要的操作步骤数
+	/*使用 二维数组 DP[i][j]，表示 word1 的 i 个字母 与 word2 的 第 j 个字母 相同 需要的操作步骤数
         将最对 word1 处理 转化为 对 word1 和 word2 均处理*/
     for(var i = 0; i <= l1; i++) dp[i][0] = i;
     for(var j = 0; j <= l2; j++) dp[0][j] = j;
